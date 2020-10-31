d334b9b5393b4ad699e76816851eed93184c5c0c
wilmerbaby <wilmerbaby@gmail.com>
2020-10-30 21:50:07 -0400

    added table from bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import  Navbar from "./components/Navbar"
+import  Navbar from "./components/Navbar";
+import Table from "./components/Table"
 
 function App() {
   return (
     <div className="App">
      <Navbar />
+     <Table />
     </div>
   );
 }
